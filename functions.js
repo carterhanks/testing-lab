127ed713e7910f07f925ccdca5cd0d966de3eedf
carterhanks <carterhanks@gmail.com>
2021-06-22 15:34:50 -0600

    Final Submission

--- functions.js
+++ functions.js
@@ -2,8 +2,8 @@ module.exports = {
     returnTwo: function() {
         return 2;
     },
-    greeting: function(name1, name2) {
-        return `Hello, ${name1}.`;
+    greeting: function(name) {
+        return `Hello, ${name}.`;
     },
     add: function(num1, num2) {
         return num1 + num2;
